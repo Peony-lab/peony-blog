b9d167d1a865cfa50a66ecefa5b5af8196d955bb
Peony-lab <translatorhoyeon@gmail.com>
2024-08-15 22:19:21 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,6 +10,12 @@ const CONFIG = {
     github: "Peony-lab",
     instagram: "",
   },
+    projects: [
+    {
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
+    },
+  ],
   // blog setting (required)
   blog: {
     title: "peony-log",
