b698ff31d9b19a8449959dc48d585b9672183f52
Peony-lab <translatorhoyeon@gmail.com>
2024-08-15 20:55:58 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "peony",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/apple-touch-icon.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "EN > KR Translator",
     bio: "I create happy user experience.",
     email: "translatorhoyeon@gmail.com",
