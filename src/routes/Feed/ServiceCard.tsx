f03339d7d37731682143f07a44bf1b8beb1798b7
Peony-lab <translatorhoyeon@gmail.com>
2024-08-16 00:01:51 +0900

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -19,7 +19,7 @@ const ServiceCard: React.FC = () => {
             rel="noreferrer"
             target="_blank"
           >
-            <div className="name"><Emoji>🔎</Emoji>{project.name}</div>
+            <div className="name">{project.name}</div>
           </a>
         ))}
       </StyledWrapper>
